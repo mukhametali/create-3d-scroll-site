79bde16b1eeb3212a2679f0e6805f73d09989546
Mikhametali <m.kussaiynov@sdu.edu.kz>
2022-10-04 18:39:59 +0600

    js for audio

--- js/app.js
+++ js/app.js
@@ -27,3 +27,19 @@ frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
 }
 
 window.scrollTo(0, 1) 
+
+
+//Audio
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+
+soundButton.addEventListener('click', e =>{
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
